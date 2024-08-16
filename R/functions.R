13899fa23f93114edb71c784e533d09b23bc6c91
andredot <andrea.pedot@ubep.unipd.it>
2024-08-16 21:04:18 +0300

    Preprocessing functions completed and integrated into targets

--- R/functions.R
+++ R/functions.R
@@ -28,7 +28,51 @@ import_data <- function(.data_path) {
     readr::read_csv()
 }
 
+import_shape <- function(.data_path) {
+  file.path(.data_path) |>
+    normalizePath() |>
+    sf::read_sf()
+}
+
+#' Preprocessing of Yemen Cholera cases
+#'
+#' @param db : dataframe obtained by the Yemen Cholera cases csv
+#'
+#' @return a new db with
+#' - clean column names,
+#' - dates subdivided by epidemiological week and year,
+#' - with all epi variables grouped by week,
+#' - government names that are the same as OCHA adm boundaries
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   preprocess_ycc(ycc)
+#' }
 preprocess_ycc <- function(db) {
+
+  ## Names aligned with the OCHA Shapefile
+  join_dict <- data.frame(
+    csv = c("Abyan", "Aden", "Al-Hudaydah", "Al Bayda", "Al Dhale'e",
+            "Al Hudaydah", "Al Jawf", "Al Maharah", "AL Mahrah",
+            "Al Mahwit", "Al_Jawf", "Amanat Al Asimah", "Amran",
+            "Dhamar", "Hajjah", "Ibb", "Lahj",
+            "Ma'areb", "Marib", "Moklla", "Raymah",
+            "Sa'ada", "Sana'a", "Say'on", "Shabwah",
+            "Taizz",
+            "Socotra", "Sana'a", "Hadramawt"),
+    shp = c("Abyan", "Aden", "Al Hodeidah", "Al Bayda", "Ad Dali'",
+            "Al Hodeidah", "Al Jawf", "Al Maharah", "Al Maharah",
+            "Al Mahwit", "Al Jawf", NA, "Amran",
+            "Dhamar", "Hajjah", "Ibb", "Lahj",
+            "Ma'rib", "Ma'rib", NA, "Raymah",
+            "Sa'dah", "Sana'a", NA, "Shabwah",
+            "Ta'iz",
+            "Socotra", "Sana'a City", "Hadramawt"
+    )
+  )
+
   db |>
     ## Change Column names
     dplyr::select(-c("COD Gov English",
@@ -45,17 +89,54 @@ preprocess_ycc <- function(db) {
     ## Group by Week
     dplyr::mutate(govt = as.factor(govt),
                   epiw = lubridate::epiweek(date),
-                  epiy = lubridate::epiyear(date)) |>
-    dplyr::group_by(epiy,epiw) |>
-    dplyr::reframe(govt = govt,
-                   date = max(date),
+                  epiy = lubridate::epiyear(date),
+                  epi_date = paste0(epiy,"-",epiw)) |>
+    dplyr::group_by(epi_date, govt) |>
+    dplyr::reframe(date = max(date),
                    cases = max(cases),
                    deaths = max(deaths),
                    cfr_abs = max(deaths),
-                   attack_abs = max(attack_abs)) |>
-    dplyr::distinct()
+                   attack_abs = max(attack_abs)
+                   ) |>
+    dplyr::distinct() |>
+    ## Adapt names to SHP ones
+    dplyr::left_join(join_dict,
+                     by = dplyr::join_by(govt == csv),
+                     relationship = "many-to-many") |>
+    dplyr::select(-govt) |>
+    dplyr::rename(govt = shp)
 }
 
-relevant_computation <- function(db) {
-  2 * length(db) + 1
+preprocess_shp <- function(db) {
+  db |>
+    dplyr::select(ADM1_EN, ADM1_PCODE, ADM1REF_EN)
+}
+
+preprocess_split <- function(db, col) {
+  db |>
+    dplyr::select(epi_date,govt,{{col}}) |>
+    dplyr::filter(!is.na(govt)) |>
+    tidyr::pivot_wider(names_from = epi_date,
+                       values_from = {{col}},
+                       names_prefix = paste0({{col}},"-"))
+}
+
+preprocess_join <- function(shp, db) {
+  require(sf)
+  dplyr::left_join(
+    shp, db,
+    by = dplyr::join_by(ADM1_EN == govt)
+    )
+}
+
+join_wider <- function(db1, db2, db3, db4) {
+  db1 |>
+    dplyr::left_join(db2, by = dplyr::join_by(govt)) |>
+    dplyr::left_join(db3, by = dplyr::join_by(govt)) |>
+    dplyr::left_join(db4, by = dplyr::join_by(govt))
+}
+
+adj_list <- function(shp, .data_path) {
+  spdep::poly2nb(shp) |>
+    spdep::nb2WB()
 }
--- _targets.R
+++ _targets.R
@@ -1,4 +1,5 @@
 library(targets)
+library(tarchetypes)
 # This is an example _targets.R file. Every
 # {targets} pipeline needs one.
 # Use tar_script() to create _targets.R and tar_edit()
@@ -19,7 +20,25 @@ tar_option_set(packages = "utils") # nolint
 
 # End this file with a list of target objects.
 list(
+
+  ## Input
   tar_target(data_raw, import_data("./data-raw/yemen_cholera_cases.csv")),
+  tar_target(shp_raw, import_shape("./data-raw/yemen_shp/yem_admbnda_adm1_govyem_cso_20191002.shp")),
+
+  # Preprocessing
   tar_target(ycc, preprocess_ycc(data_raw)),
-  tar_target(data_summary, summarize_data(ycc)) # Call your custom functions.
+  tar_target(data_summary, summarize_data(ycc)),
+  tar_target(yem_shp, preprocess_shp(shp_raw)),
+  tar_target(cases, preprocess_split(ycc, "cases")),
+  tar_target(deaths, preprocess_split(ycc, "deaths")),
+  tar_target(cfr_abs, preprocess_split(ycc, "cfr_abs")),
+  tar_target(attack_abs, preprocess_split(ycc, "attack_abs")),
+  tar_target(yccwider, join_wider(cases, deaths,cfr_abs,attack_abs)),
+  tar_target(yemen, preprocess_join(yem_shp, yccwider)),
+
+  ## Save files
+  tar_target(adj_list_out,
+             dput(adj_list(yem_shp),
+                  file = "./data-raw/adj_list.txt",
+                  control = "niceNames"))
 )
